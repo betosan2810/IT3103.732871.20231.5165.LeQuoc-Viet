7c2f99236c72f103e96e38574f89b4fa3423f7aa
Le Viet <vle80444@gmail.com>
2023-11-14 00:34:23 +0700

    Print Cart

--- lab03_java/Cart.java
+++ lab03_java/Cart.java
@@ -1,64 +1,138 @@
-//Le Quoc Viet - 20215165
 package lab03_java;
 
 public class Cart {
-    public static final int MAX_NUMBERS_ORDERED = 20;
-
-    // init cart
+public static final int MAX_NUMBERS_ORDERED = 20;
+    
+    // khởi tạo giỏ hàng
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-    // init so dia them vao cart
+    // khởi tạo số đĩa đã thêm vào giỏ
     private int qtyOrdered;
-
-    // them dia vao cart
+    
+    // hàm thêm đĩa vào giỏ hàng
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        // chua day cart thi them dia vao
+    	// nếu giỏ chưa đầy thì thêm đĩa vào
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
             System.out.println("The disc has been added.");
         } else {
-            // cart day thi in ra thong bao k them duoc
+        	// giỏ đầy thì không thêm
             System.out.println("The cart is almost full. Cannot add more discs.");
         }
     }
 
+    // New method with a different type of parameter (array of DVDs)
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        // Add a list of DVDs to the current cart
+        for (DigitalVideoDisc dvd : dvdList) {
+            addDigitalVideoDisc(dvd); // Reuse the existing method
+        }
+    }
+
+    // // Another method allowing an arbitrary number of DVD arguments
+    // public void addDigitalVideoDisc(DigitalVideoDisc... dvdArray) {
+    //     // Add an arbitrary number of DVDs to the current cart
+    //     for (DigitalVideoDisc dvd : dvdArray) {
+    //         addDigitalVideoDisc(dvd); // Reuse the existing method
+    //     }
+    // }
+
+    // Overloading by differing the number of parameters
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        // Add two DVDs to the current cart
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
+    }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+    	// khởi tạo biến found 
         boolean found = false;
         for (int i = 0; i < qtyOrdered; i++) {
-            // tim dia can xoa trong cart
+        	// tìm thấy đĩa cần xóa trong giỏ hàng
             if (itemsOrdered[i].equals(disc)) {
-                // tang chi so cua dia dung sau len 1 don vi
+                // nâng chỉ số của các đĩa đứng sau đĩa vừa tìm thấy lên 1
                 for (int j = i; j < qtyOrdered - 1; j++) {
                     itemsOrdered[j] = itemsOrdered[j + 1];
                 }
-                // xoa dia o vi tri do
-                itemsOrdered[qtyOrdered - 1] = null; // gan null cho phan tu cuoi
+                // xóa đĩa ở vị trí đó
+                itemsOrdered[qtyOrdered - 1] = null; // Set the last element to null
                 qtyOrdered--;
                 found = true;
                 System.out.println("The disc has been removed.");
-                break; // thoat chuong trinh va xoa dia
+                break; // thoát chương trình khi đã tìm và xóa đĩa
             }
         }
-        // khong tim thay dia
+        // không tìm thấy đĩa
         if (!found) {
             System.out.println("Disc not found in the cart. Cannot remove.");
         }
     }
-
-    // tinh tong tien
+    
+    // hàm tính tổng tiền
     public float totalCost() {
-        // init total
+    	// khởi tạo biến total
         float total = 0.0f;
-        // duyet qua cart de tinh tong
+        // lặp qua giỏ hàng để cộng các cost lại
         for (int i = 0; i < qtyOrdered; i++) {
             total += itemsOrdered[i].getCost();
         }
         return total;
     }
+    
+    // New method to print the list of ordered items, their details, and the total cost
+    public void printCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:\n");
 
-    // in thong tin cac dia con lai trong gio
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". DVD - " + itemsOrdered[i].toString());
+        }
+
+        System.out.println("\nTotal cost: " + totalCost() + " $\n");
+        System.out.println("***************************************************");
+    }
+
+    // New method to search for DVDs in the cart by ID and display the search results
+    public void searchByID(int id) {
+        System.out.println("Search Result for DVD with ID " + id + ":");
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("DVD - " + itemsOrdered[i].toString());
+                return; // Found a match, no need to continue searching
+            }
+        }
+
+        System.out.println("No match found for DVD with ID " + id);
+    }
+
+    // New method to search for DVDs in the cart by title and print the results
+    public void searchByTitle(String title) {
+        System.out.println("Search Result for DVDs with Title '" + title + "':");
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("DVD - " + itemsOrdered[i].toString());
+            }
+        }
+
+        if (noMatchFound(title)) {
+            System.out.println("No match found for DVDs with Title '" + title + "'");
+        }
+    }
+
+    // Helper method to check if there is no match for the given title
+    private boolean noMatchFound(String title) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                return false; // Found a match, no need to continue checking
+            }
+        }
+        return true; // No match found
+    }
+    // hàm in thông tin tất cả các đĩa trong giỏ hàng (để kiểm tra sau khi xóa đĩa khỏi giỏ)
     public String toString() {
-        String result = "";
+    	String result = "";
         for (int i = 0; i < qtyOrdered; i++) {
             result += itemsOrdered[i].toString();
             result += "\n";
--- lab03_java/DigitalVideoDisc.java
+++ lab03_java/DigitalVideoDisc.java
@@ -2,35 +2,34 @@
 package lab03_java;
 
 public class DigitalVideoDisc {
+	private static int nbDigitalVideoDiscs = 0;
+
 	private String title;
 	private String category;
 	private String director;
 	private int length;
-	private float cost;
+	private float c
+
 	
-	//Cac ham constructer
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
-	}
-	
+		
+
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
-		this.cost = cost;
-	}
+		
 
 	
-
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.director = director;
-		this.cost = cost;
-	}
+		
 
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -39,33 +38,70 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.length = length;
-		this.cost = cost;
-	}
+		
 
-	public String getTitle() {
-		return title;
-	}
+	
 	public void setTitle(String title) {
-		this.title = title;
+		
+
+	
+	public void setCategory(String category) {
+		
+
+	
+	public void setDirector(String director) {
+		
+
+	
+	public void setLength(int length) {
+		
+
+	
+	public void setCost(float cost) {
+		
+
+	
+	public String getTitle() {
+		
+
 	}
-
-
 	public String getCategory() {
-		return category;
+		
+
 	}
 	public String getDirector() {
-		return director;
+		
+
 	}
 	public int getLength() {
-		return length;
+		
+
 	}
 	public float getCost() {
 		return cost;
 	}
+
+	Additional getter fo
+		ic int get
+	    return id;
+    }
+
+	// 
+	Helper method to update class attribu
+		ate void updateDiscCou
+		nbDigitalVideoDiscs++;
+	    id = nbDigitalVideoDiscs;
+    }
+
+	New method to check if the title matches
+		ic boolean isMatch(String pattern) {
+		// Use the contains method to check if the title contains t
+	 
+
 	
 	// ham in ra thong tin dia
-	public String toString() {
-        return "Title: " + title + ", Cost: $" + cost;
-    }
+		String toString() {
+	 
+
 
 }
