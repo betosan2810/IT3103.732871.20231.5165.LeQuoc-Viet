d766e136ee11d50ce9de3c2926a0b1dfb8caed9d
Le Viet <vle80444@gmail.com>
2023-12-28 14:43:11 +0700

    lab05

---
+++ lab05_java/hust/soict/dsai/aims/media/MediaComparatorByCost.java
@@ -0,0 +1,12 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.Comparator;
+
+public class MediaComparatorByCost implements Comparator<Media> {
+
+	@Override
+	public int compare(Media o1, Media o2) {
+		return Float.floatToIntBits(o2.getCost()) - Float.floatToIntBits(o1.getCost());
+	}
+
+}
